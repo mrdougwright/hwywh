659e205ee9620e9a58c662871c858d5b5ce3767b
Doug Wright <mrdougwright@gmail.com>
2013-05-14 12:33:29 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hwywh::Application.config.secret_token = '0643523bff4025994226c77902c19442bd371de86d5d13f1f3778032366dfedd0959a1a024c9975ed8a98d1806ddd0e24b3ba998cf93d8e810591b37866cfd64'
